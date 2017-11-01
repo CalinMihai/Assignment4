6b8bf125c6363006351c04c250dfe6e42f16eeed
Calin Mihai <32879010+CalinMihai@users.noreply.github.com>
2017-11-01 09:19:13 +0200

    Add files via upload

---
+++ I.5.b).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int i;
+int main(){
+int i = 2;
+    while(i != 60){
+        printf("%d, ", i);
+        i += 2;
+    }
+    printf("%d.", 60);
+    return 0;
+}
