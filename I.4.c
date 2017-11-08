1bed6299b1bddc094a56f2c1f2b21a0cf893d2e5
Calin Mihai <32879010+CalinMihai@users.noreply.github.com>
2017-11-08 09:12:31 +0200

    Update I.4.c

--- I.4.c
+++ I.4.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 int a, b;
-void swap (int &x, int &y)
+void swap (int *x, int *y)
 {
-    int t=x;
-    x=y;
-    y=t;
+    int t=*x;
+    *x=*y;
+    *y=t;
 }
 
 int main () {
    scanf("%d%d", &a,&b);
    printf("Before swap, value of a: %d\n", a);
    printf("Before swap, value of b: %d\n", b);
-   swap(a,b);
+   swap(&a,&b);
    printf("After swap, value of a: %d\n", a);
    printf("After swap, value of b: %d\n", b);
    return 0;
