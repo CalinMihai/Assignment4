b9f354b581e407c6b521ff679a36e57e3cb1f2d8
Calin Mihai <32879010+CalinMihai@users.noreply.github.com>
2017-11-08 09:21:11 +0200

    Update I.3.c

--- I.3.c
+++ I.3.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 int x, y, z, a, b;
 double c;
-void sum (int x, int y, int z, int &a)
+void sum (int x, int y, int z, int *a)
 {
-    a=x=y+z;
+    *a = x + y + z;
 }
-void prod (int x, int y, int z, int &a)
+void prod (int x, int y, int z, int *b)
 {
-    a=x*y*z;
+    *b = x * y * z;
 }
-void avg (int x, int y, int z, double &a)
+void avg (int x, int y, int z, double *c)
 {
-    a=(x+y+z)/3.0;
+    *c = (x+y+z) /3.0;
 }
 int main (){
     printf("Input the 3 numbers: ");
-    scanf("%x %y %z", x, y, z);
-    sum(x,y,z,a);
-    prod(x,y,z,b);
-    avg(x,y,z,c);
-    printf("The sum is: %a\n", a);
-    printf("The product is: %b\n", b);
-    printf("The average is: %c\n", c);
+    scanf("%d%d%d", &x,&y,&z);
+        sum(x,y,z,&a);
+        prod(x,y,z,&b);
+        avg(x,y,z,&c);
+    printf("The sum is: %d\n", a);
+    printf("The product is: %d\n", b);
+    printf("The average is: %lf\n", c);
     return 0;
 }
